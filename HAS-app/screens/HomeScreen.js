0a571f6ddbade8807f593a51910248fbb0fe631a
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-05-07 16:34:15 +0100

    adds conditional rendering of the home page when the add job button is pressed. placeholder for the job number input to render on addJob

--- HAS-app/screens/HomeScreen.js
+++ HAS-app/screens/HomeScreen.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-        title: navigation.getParam('email', '')
-    }
+      headerTitle: navigation.getParam('email', ''),
+      headerRight: <Button onPress={navigation.getParam('addJob')} title="+" />
+    };
   };
+  componentDidMount() {
+    this.props.navigation.setParams({ addJob: this.handleAddJob });
+  }
+  state = {
+    jobNumber: '',
+    addJob: false
+  };
+  handleAddJob = () => {
+    this.setState({ addJob: true });
+  };
+  handleJobNumber = text => {
+    this.setState({ jobNumber: text });
+  };
+
+  handleAddJobSubmit = () => {
+    this.setState({ addJob: false });
+  }
   render() {
     const { navigation } = this.props;
     const email = navigation.getParam('email', '');
-    return (
+    return !this.state.addJob ? (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <Text>user email: {JSON.stringify(email)}</Text>
+        <Text>job number: {this.state.jobNumber}</Text>
         <Button
           title="Go to job"
           onPress={() => this.props.navigation.navigate('Job')}
         />
       </View>
+    ) : (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Find a job</Text>
+        <TextInput
+          placeholder="Job Number"
+          keyboardType="numeric"
+          style={styles.input}
+          onChangeText={this.handleJobNumber}
+        />
+        <Button
+          title="Submit"
+          onPress={this.handleAddJobSubmit}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    width: 200,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  }
+});
