1fdc3cd791ee63bf1de1610200674219efed2e61
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-05-07 15:34:01 +0100

    sets out the login page and passes the username to the title of the home page after login

--- HAS-app/screens/HomeScreen.js
+++ HAS-app/screens/HomeScreen.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Home',
-    };
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Home Screen</Text>
-          <Button
-            title="Go to job"
-            onPress={() => this.props.navigation.navigate('Job')}
-          />
-        </View>
-      );
+  static navigationOptions = ({navigation}) => {
+    return {
+        title: navigation.getParam('email', '')
     }
+  };
+  render() {
+    const { navigation } = this.props;
+    const email = navigation.getParam('email', '');
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Text>user email: {JSON.stringify(email)}</Text>
+        <Button
+          title="Go to job"
+          onPress={() => this.props.navigation.navigate('Job')}
+        />
+      </View>
+    );
   }
+}
